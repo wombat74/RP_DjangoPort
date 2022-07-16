09168fba0c8453a657ee2d656be46f3a2eeb8caf
Eddie Gonzalez <eddie_gonzalez@Eddies-MBP.fios-router.home>
2022-07-16 10:17:03 -0700

    added project_detial view

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,5 @@ from projects import views
 
 urlpatterns = [
     path('', views.all_projects),
+    path('<int:pk>', views.project_detail),
 ]
--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,7 @@ def all_projects(request):
     # query db to return all projects objects
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
